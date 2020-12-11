066b7f91d7bef0ac2da025065e5a5bf121642bdd
bt <biantaobrad@gmail.com>
2020-12-11 10:20:55 +0800

    initial commit

---
+++ src/router/myRouter.js
@@ -0,0 +1,56 @@
+let Vue = null
+
+class MyVueRouter {
+  constructor (options) {
+    this.$options = options
+
+    this.routeMap = this.$options.routes
+
+    Vue.util.defineReactive(this, 'current', '/')
+
+    window.addEventListener('hashchange', this.onHashChange.bind(this))
+    window.addEventListener('load', this.onHashChange.bind(this))
+  }
+
+  onHashChange () {
+    this.current = window.location.hash.slice(1)
+  }
+}
+
+MyVueRouter.install = function (_vue) {
+  Vue = _vue
+
+  Vue.mixin({
+    beforeCreate () {
+      if (this.$options.router) {
+        Vue.prototype.$router = this.$options.router
+      }
+    }
+  })
+
+  Vue.component('router-link', {
+    props: {
+      to: {
+        type: String,
+        default: '/'
+      }
+    },
+    render () {
+      return <a href={`#${this.to}`}>{this.$slots.default}</a>
+    }
+  })
+
+  Vue.component('router-view', {
+    render (h) {
+      const { current, routeMap } = this.$router
+      const component = routeMap.filter(route => {
+        if (route.path === `${current}`) {
+          return true
+        }
+      })[0].component
+      return h(component)
+    }
+  })
+}
+
+export default MyVueRouter
