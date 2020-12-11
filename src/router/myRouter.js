88d00c18daa5f34b5710155df4ac522f31c21df3
bt <biantaobrad@gmail.com>
2020-12-11 10:43:32 +0800

    添加注释

--- src/router/myRouter.js
+++ src/router/myRouter.js
@@ -1,17 +1,20 @@
 import myRouterLink from './myRouterLink'
 import myRouterView from './myRouterView'
 
+// 为了使用Vue中的响应式工具函数
 let Vue = null
 
 class MyVueRouter {
   constructor (options) {
     this.$options = options
 
+    // 保存路由表配置，避免每次都去循环遍历
     this.routeMap = {}
     this.$options.routes.forEach(route => {
       this.routeMap[route.path] = route.component
     })
 
+    // vue源码中有相应的工具方法，只要在render中使用了响应式的数据，就会进行依赖收集
     Vue.util.defineReactive(this, 'current', '/')
 
     window.addEventListener('hashchange', this.onHashChange.bind(this))
@@ -26,6 +29,11 @@ class MyVueRouter {
 MyVueRouter.install = function (_vue) {
   Vue = _vue
 
+  /**
+   * 因为先Vue.use使用router插件,
+   * 再创建根组件实例,
+   * 所以通过mixin一个beforeCreate钩子来添加$router方法
+   */
   Vue.mixin({
     beforeCreate () {
       if (this.$options.router) {
